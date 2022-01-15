b4d175afda23944a2a5c48f133c4c2d350f843e0
takinabradley <bradfinney98@gmail.com>
2022-01-15 02:32:44 -0500

    added transitions and auto-change on image carousel

--- index.js
+++ index.js
@@ -3,33 +3,42 @@ function carouselImages() {
   const totalImages = images.length;
   const nextBtn = document.querySelector(".carousel-next");
   const previousBtn = document.querySelector(".carousel-previous");
+  const displayImage = () => {
+    images.forEach((image) => {
+      if (
+        image !==
+        document.querySelector(`.carousel-images img:nth-child(${currentImg})`)
+      ) {
+        image.style.visibility = "hidden";
+        image.classList.remove('active');
+      } else {
+        image.style.visibility = "initial";
+        image.classList.add('active');
+      }
+    });
+  }
 
   let currentImg = 1;
-  displayImage(images, currentImg);
+  displayImage();
 
-  nextBtn.addEventListener("click", () => {
+  const intervalID = setInterval(() => {
     currentImg++;
     if (currentImg > totalImages) currentImg = 1;
-    displayImage(images, currentImg);
+    displayImage();
+  }, 5000)
+
+  nextBtn.addEventListener("click", () => {
+    clearInterval(intervalID)
+    currentImg++;
+    if (currentImg > totalImages) currentImg = 1;
+    displayImage();
   });
 
   previousBtn.addEventListener("click", () => {
+    clearInterval(intervalID)
     currentImg--;
     if (currentImg < 1) currentImg = totalImages;
-    displayImage(images, currentImg);
-  });
-}
-
-function displayImage(images, currentImg) {
-  images.forEach((image) => {
-    if (
-      image !==
-      document.querySelector(`.carousel-images img:nth-child(${currentImg})`)
-    ) {
-      image.style.display = "none";
-    } else {
-      image.style.display = "initial";
-    }
+    displayImage();
   });
 }
 
