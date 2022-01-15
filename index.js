c0efab86d2e9dbc80228cfcaa6ed74aa029dfbb4
takinabradley <bradfinney98@gmail.com>
2022-01-15 13:13:57 -0500

    Added numbers that indicate which image is being displayed, and allow quick switching to a specific image to the image carousel

--- index.js
+++ index.js
@@ -3,43 +3,75 @@ function carouselImages() {
   const totalImages = images.length;
   const nextBtn = document.querySelector(".carousel-next");
   const previousBtn = document.querySelector(".carousel-previous");
+  const indicatorContainer = document.querySelector(".carousel-indicators");
+
+  const indicators = (() => {
+    for (let i = 1; i < totalImages + 1; i++) {
+      let indicator = document.createElement("button");
+      indicator.classList.add("carousel-indicator");
+      indicator.textContent = i;
+      indicatorContainer.appendChild(indicator);
+    }
+    return indicatorContainer.querySelectorAll(".carousel-indicator");
+  })();;
+
+  const currentIndicator = () => {
+    return indicatorContainer.querySelector(`button:nth-child(${currentImg})`);
+  };
+
+  let currentImg = 1;
+
+  
   const displayImage = () => {
     images.forEach((image) => {
       if (
-        image !==
+        image ===
         document.querySelector(`.carousel-images img:nth-child(${currentImg})`)
       ) {
-        image.style.visibility = "hidden";
-        image.classList.remove('active');
-      } else {
         image.style.visibility = "initial";
-        image.classList.add('active');
+        image.classList.add("active");
+        currentIndicator().classList.add("active");
+      } else {
+        image.style.visibility = "hidden";
+        image.classList.remove("active");
+        indicators.forEach((indicator) => {
+          if (indicator !== currentIndicator()) {
+            indicator.classList.remove("active");
+          }
+        });
       }
     });
-  }
+  };
 
-  let currentImg = 1;
   displayImage();
 
   const intervalID = setInterval(() => {
     currentImg++;
     if (currentImg > totalImages) currentImg = 1;
     displayImage();
-  }, 5000)
+  }, 5000);
 
   nextBtn.addEventListener("click", () => {
-    clearInterval(intervalID)
+    clearInterval(intervalID);
     currentImg++;
     if (currentImg > totalImages) currentImg = 1;
     displayImage();
   });
 
   previousBtn.addEventListener("click", () => {
-    clearInterval(intervalID)
+    clearInterval(intervalID);
     currentImg--;
     if (currentImg < 1) currentImg = totalImages;
     displayImage();
   });
+
+  indicators.forEach((indicator) =>
+    indicator.addEventListener("click", (e) => {
+      clearInterval(intervalID);
+      currentImg = e.target.textContent;
+      displayImage();
+    })
+  );
 }
 
 carouselImages();
